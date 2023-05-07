6a96da5fe0dfd7c889d143c8f56fac636b5d0c3d
Noah Kilker <kilke023@umn.edu>
2023-05-07 15:35:51 -0500

    Creates necessary functions for cart

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -5,6 +5,18 @@ import { Product } from './products';
   providedIn: 'root'
 })
 export class CartService {
+  items: Product[] = [];
 
-  constructor() { }
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
